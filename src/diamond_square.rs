f560304be63c765b3e1e7048efb7eba6674e9aae
maccod <maccora17@gmail.com>
2016-11-27 10:24:22 +1100

    Modified structure and added enumeration of pixels. Added first draw of the generated map.

--- src/diamond_square.rs
+++ src/diamond_square.rs
@@ -4,36 +4,90 @@ use self::rand::Rng;
 
 // This value determines if the terrain is smooth or mountainous. 0=smooth 1=mountainous
 const ROUGHNESS: f32 = 0.8;
+// NOTE Look into using the chunks like the image library
 type Buffer = Vec<f32>;
 
+struct EnumeratePixelMap {
+    // pixels: Buffer,
+    x: u32,
+    y: u32,
+    size: u32,
+}
+
+impl Iterator for EnumeratePixelMap {
+    type Item = (u32, u32);
+    fn next(&mut self) -> Option<(u32, u32)> {
+        if self.x >= self.size {
+            self.x = 0;
+            self.y += 1;
+        }
+        let (x, y) = (self.x, self.y);
+        self.x += 1;
+        if self.y >= self.size {
+            None
+        } else {
+            Some((x, y))
+        }
+    }
+}
+
 /// Structure containing the essential elements to perform the Diamond-Square algorithm
-struct DSAlgorithm {
+pub struct PixelMap {
     map: Buffer,
-    size: usize,
-    max: i32,
+    size: u32,
+}
+
+impl PixelMap {
+    fn max(&self) -> i32 {
+        (self.size - 1) as i32
+    }
+
+    pub fn get_pixel(&self, x: u32, y: u32) -> f32 {
+        let x = x as usize;
+        let y = y as usize;
+        self.map[x + y * (self.size as usize)]
+    }
+
+    fn set_pixel(&mut self, x: u32, y: u32, value: f32) {
+        let x = x as usize;
+        let y = y as usize;
+        self.map[x + y * (self.size as usize)] = value;
+    }
+
+    fn size(&self) -> u32 {
+        self.size
+    }
+
+    fn enumerate_pixels(&self) -> EnumeratePixelMap {
+        EnumeratePixelMap {
+            x: 0,
+            y: 0,
+            size: self.size, // pixels: self.pixels(),
+        }
+    }
 }
 
 /// Constructs a Buffer of with dimensions size * size
-pub fn construct(size: usize) -> Buffer {
-    let mut ds = DSAlgorithm {
-        map: vec![0.0f32; size * size],
+pub fn construct(size: u32) -> PixelMap {
+    let size_vec = size as usize;
+    let mut ds = PixelMap {
+        map: vec![0.0f32; size_vec * size_vec],
         size: size,
-        max: (size - 1) as i32,
     };
     const INITIAL: f32 = 100.0;
     // Set all corner points to a seed value
     // Then perform steps alternatively until all array values are set
-    for x_coord in vec![0, ds.max] {
-        for y_coord in vec![0, ds.max] {
+    for x_coord in vec![0, ds.max()] {
+        for y_coord in vec![0, ds.max()] {
             set_sample(&mut ds, x_coord, y_coord, INITIAL);
         }
     }
-    let initial_size = ds.max;
+    let initial_size = ds.max();
     divide(&mut ds, initial_size);
-    ds.map
+    ds
 }
 
-fn divide(ds: &mut DSAlgorithm, size: i32) {
+fn divide(ds: &mut PixelMap, size: i32) {
     let half = size / 2;
     if half < 1 {
         return;
@@ -43,15 +97,15 @@ fn divide(ds: &mut DSAlgorithm, size: i32) {
     divide(ds, size / 2);
 }
 
-fn diamond_step(ds: &mut DSAlgorithm, feature_size: i32) {
+fn diamond_step(ds: &mut PixelMap, feature_size: i32) {
     let half_step = feature_size / 2;
     // By multiplying by size we ensure that it shrinks with the feature size
     let size = feature_size as f32;
     let scale = ROUGHNESS * size;
     let mut y: i32 = 0;
-    while y <= ds.max {
+    while y <= ds.max() {
         let mut x: i32 = (y + half_step) % feature_size;
-        while x <= ds.max {
+        while x <= ds.max() {
             diamond_sample(ds,
                            x,
                            y,
@@ -63,15 +117,15 @@ fn diamond_step(ds: &mut DSAlgorithm, feature_size: i32) {
     }
 }
 
-fn square_step(ds: &mut DSAlgorithm, feature_size: i32) {
+fn square_step(ds: &mut PixelMap, feature_size: i32) {
     let half_step = feature_size / 2;
     // By multiplying by size we ensure that it shrinks with the feature size
     let size = feature_size as f32;
     let scale = ROUGHNESS * size;
     let mut y = half_step;
-    while y < ds.max {
+    while y < ds.max() {
         let mut x = half_step;
-        while x < ds.max {
+        while x < ds.max() {
             square_sample(ds,
                           x,
                           y,
@@ -84,7 +138,7 @@ fn square_step(ds: &mut DSAlgorithm, feature_size: i32) {
 }
 
 /// Calculate the centre of the diamond using the average, plus a random value
-fn diamond_sample(ds: &mut DSAlgorithm, x: i32, y: i32, rand_value: f32, size: i32) {
+fn diamond_sample(ds: &mut PixelMap, x: i32, y: i32, rand_value: f32, size: i32) {
     //   a
     // b x c
     //   d
@@ -96,7 +150,7 @@ fn diamond_sample(ds: &mut DSAlgorithm, x: i32, y: i32, rand_value: f32, size: i
     set_sample(ds, x, y, value);
 }
 
-fn square_sample(ds: &mut DSAlgorithm, x: i32, y: i32, rand_value: f32, size: i32) {
+fn square_sample(ds: &mut PixelMap, x: i32, y: i32, rand_value: f32, size: i32) {
     // a  b
     //  x
     // c  d
@@ -108,11 +162,11 @@ fn square_sample(ds: &mut DSAlgorithm, x: i32, y: i32, rand_value: f32, size: i3
     set_sample(ds, x, y, value);
 }
 
-fn sample(ds: &mut DSAlgorithm, x: i32, y: i32) -> f32 {
+fn sample(ds: &mut PixelMap, x: i32, y: i32) -> f32 {
     let grid_size = ds.size as i32;
-    let x = modulo(x, grid_size) as usize;
-    let y = modulo(y, grid_size) as usize;
-    ds.map[x + y * ds.size]
+    let x = modulo(x, grid_size) as u32;
+    let y = modulo(y, grid_size) as u32;
+    ds.get_pixel(x, y)
 }
 
 fn modulo(x: i32, modulo: i32) -> i32 {
@@ -120,8 +174,34 @@ fn modulo(x: i32, modulo: i32) -> i32 {
 }
 
 /// Set the value of the given element in the map
-fn set_sample(ds: &mut DSAlgorithm, x: i32, y: i32, value: f32) {
-    let x = x as usize;
-    let y = y as usize;
-    ds.map[x + y * ds.size] = value;
+fn set_sample(ds: &mut PixelMap, x: i32, y: i32, value: f32) {
+    let x = x as u32;
+    let y = y as u32;
+    ds.set_pixel(x, y, value)
+}
+
+
+pub fn normalize_pixel_map(p_map: PixelMap) -> PixelMap {
+    let size = p_map.size() as usize;
+    let mut new_p_map = PixelMap {
+        map: vec![0.0f32; size* size],
+        size: p_map.size(),
+    };
+    // TODO Check that we are always setting value to positive
+    let mut max_value = 0.0;
+    for (x, y) in p_map.enumerate_pixels() {
+        let f_value = p_map.get_pixel(x, y);
+        if f_value > max_value {
+            max_value = f_value;
+        }
+    }
+
+    for (x, y) in p_map.enumerate_pixels() {
+        let mut f_value = p_map.get_pixel(x, y);
+        f_value /= max_value;
+        f_value *= 255.0;
+        new_p_map.set_pixel(x, y, f_value);
+    }
+
+    new_p_map
 }
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+extern crate rusty_flow;
+extern crate image;
+
+use std::path::Path;
+use std::fs::File;
+
+const IMAGE_SIZE: u32 = 800;
+fn main() {
+    let buffer = rusty_flow::diamond_square::construct(IMAGE_SIZE);
+    let buffer = rusty_flow::diamond_square::normalize_pixel_map(buffer);
+
+    let mut img_buf = image::ImageBuffer::new(IMAGE_SIZE as u32, IMAGE_SIZE as u32);
+
+    for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
+        *pixel = image::Luma([buffer.get_pixel(x, y) as u8]);
+    }
+
+    // let mut i = 0;
+    // for elem in &buffer {
+    //     print!("{}", elem);
+    //     i += 1;
+    //     if i == IMAGE_SIZE {
+    //         println!("", );
+    //         i = 0;
+    //     }
+    // }
+
+    // Save the image as “fractal.png”
+    let ref mut fout = File::create(&Path::new("fractal.png")).unwrap();
+
+    // We must indicate the image’s color type and what format to save as
+    let _ = image::ImageLuma8(img_buf).save(fout, image::PNG);
+}
